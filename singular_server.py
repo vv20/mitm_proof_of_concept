50beec20fc376e5675d7cfd2330983b0f90da5ac
victor <vv20@st-andrews.ac.uk>
2017-08-15 11:14:44 +0100

    concentrating on singular server now

---
+++ singular_server.py
@@ -0,0 +1,143 @@
+import socket
+import socketserver
+import logging
+from openmath import openmath as om, convert as conv
+
+from scscp.client import TimeoutError, CONNECTED
+from scscp.server import SCSCPServer
+from scscp.scscp import SCSCPQuit, SCSCPProtocolError
+from scscp import scscp
+
+import PySingular as sing
+
+def poly_eq(data):
+    print("polyeq")
+    return None
+
+def ideal(data):
+    print("ideaL")
+    return None
+
+# Supported functions
+CD_SCSCP2 = ['get_service_description', 'get_allowed_heads', 'is_allowed_head', 'get_signature']
+CD_SCSCP_TRANS = [
+        'polynomial_eq',
+        'ideal'
+]
+
+def get_handler(head):
+    if head == "ideal":
+        return ideal
+    elif head == "polynomial_eq":
+        return poly_eq
+    else:
+        return None
+
+headers = [
+        om.OMSymbol("get_service_description", "scscp2"),
+        om.OMSymbol("get_allowed_heads", "scscp2"),
+        om.OMSymbol("is_allowed_head", "scscp2"),
+        om.OMSymbol("get_signature", "scscp2"),
+        om.OMSymbol("polynomial_eq", "scscp_trans_1"),
+        om.OMSymbol("ideal", "scscp_trans_1")
+        ]
+
+class SCSCPRequestHandler(socketserver.BaseRequestHandler):
+    def setup(self):
+        self.server.log.info("New connection from %s:%d" % self.client_address)
+        self.log = self.server.log.getChild(self.client_address[0])
+        self.scscp = SCSCPServer(self.request, self.server.name,
+                                     self.server.version, logger=self.log)
+        
+    def handle(self):
+        self.scscp.accept()
+        while True:
+            try:
+                call = self.scscp.wait()
+            except TimeoutError:
+                continue
+            except SCSCPQuit as e:
+                self.log.info(e)
+                break
+            except ConnectionResetError:
+                self.log.info('Client closed unexpectedly.')
+                break
+            except SCSCPProtocolError as e:
+                self.log.info('SCSCP protocol error: %s.' % str(e))
+                self.log.info('Closing connection.')
+                self.scscp.quit()
+                break
+            self.handle_call(call)
+
+    def handle_call(self, call):
+        if (call.type != 'procedure_call'):
+            raise SCSCPProtocolError('Bad message from client: %s.' % call.type, om=call.om())
+        try:
+            head = call.data.elem.name
+            self.log.debug('Requested head: %s...' % head)
+            
+            if call.data.elem.cd == 'scscp2' and head in CD_SCSCP2:
+                res = getattr(self, head)(call.data)
+            elif call.data.elem.cd == 'scscp_trans_1' and head in CD_SCSCP_TRANS:
+                args = [conv.to_python(a) for a in call.data.arguments]
+                handler = get_handler(head)
+                res_sym = handler(args)
+                res = conv.to_openmath(res_sym)
+            else:
+                self.log.debug('...head unknown.')
+                return self.scscp.terminated(call.id, om.OMError(
+                    om.OMSymbol('unhandled_symbol', cd='error'), [call.data.elem]))
+
+            strlog = str(res)
+            self.log.debug('...sending result: %s' % (strlog[:20] + (len(strlog) > 20 and '...')))
+            return self.scscp.completed(call.id, res)
+        except (AttributeError, IndexError, TypeError) as e:
+            self.log.debug('...client protocol error.')
+            return self.scscp.terminated(call.id, om.OMError(
+                om.OMSymbol('unexpected_symbol', cd='error'), [call.data]))
+        except Exception as e:
+            self.log.exception('Unhandled exception:')
+            return self.scscp.terminated(call.id, 'system_specific',
+                                             'Unhandled exception %s.' % str(e))
+
+    def get_allowed_heads(self, data):
+        return scscp.symbol_set([om.OMSymbol(head, cd='scscp2') for head in CD_SCSCP2]
+                                    + [om.OMSymbol(head, cd='scscp_trans_1') for head in CD_SCSCP_TRANS],
+                                    cdnames=['scscp1'])
+    
+    def is_allowed_head(self, data):
+        head = data.arguments[0]
+        return conv.to_openmath((head.cd == 'scscp_trans_1' and head.name in CD_SCSCP_TRANS)
+                                    or (head.cd == 'scscp2' and head.name in CD_SCSCP2)
+                                    or head.cd == 'scscp1')
+
+    def get_service_description(self, data):
+        return scscp.service_description(self.server.name.decode(),
+                                             self.server.version.decode(),
+                                             self.server.description)
+
+    def get_signature(self, data):
+        return om.OMApplication(om.OMSymbol("symbol_set", "scscp2"), headers)
+
+class Server(socketserver.ThreadingMixIn, socketserver.TCPServer, object):
+    allow_reuse_address = True
+    
+    def __init__(self, host='localhost', port=26133,
+                     logger=None, name=b'SingularServer', version=b'0.0.1',
+                     description='Singular SCSCP Server'):
+        super(Server, self).__init__((host, port), SCSCPRequestHandler)
+        self.log = logger or logging.getLogger(__name__)
+        self.name = name
+        self.version = version
+        self.description = description
+        
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('singular_server')
+    srv = Server(logger=logger)
+    try:
+        srv.serve_forever()
+    except KeyboardInterrupt:
+        srv.shutdown()
+        srv.server_close()
+
